96d8d7cc5db42de9a6405164de0b3baf2ba8863b
CBPher24 <Christopher.b.partee@gmail.com>
2022-05-30 07:20:36 -0400

    finito

--- app.py
+++ app.py
@@ -11,25 +11,25 @@ from flask import Flask, jsonify, render_template
 # create engine to hawaii.sqlite
 db_path = os.path.join("Resources","hawaii.sqlite")
 engine = create_engine(f"sqlite:///{db_path}")
-con = engine.connect()
+# con = engine.connect()
 
 # reflect an existing database into a new model
 HI_base = automap_base()
-HI_base.metadata.create_all(con)
+HI_base.metadata.create_all(engine)
 
 # reflect the tables
-HI_base.prepare(con, reflect=True)
+HI_base.prepare(engine, reflect=True)
 
 # Save references to each table
 hi_meas = HI_base.classes.measurement
 hi_stati = HI_base.classes.station
 
 # Create our session (link) from Python to the DB
-session = Session(bind=con)
+session = Session(bind=engine)
 session
 
 # Find the most recent date in the data set.
-meas_db = pd.read_sql_query("select * from measurement", con)
+meas_db = pd.read_sql_query("select * from measurement", engine)
 date_sort = meas_db.sort_values("date", ascending=False)
 date_sort["dateind"] = pd.to_datetime(date_sort["date"])
 date_sort = date_sort.set_index("dateind")
@@ -56,45 +56,39 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    year_prec = last_year.reset_index(drop=True, inplace=True)
-    year_prec = year_prec.drop(['id', 'station', 'date', 'tobs'], axis=1)
-    prec_html = year_prec.to_json()
 
-    return prec_html
+    year_prec = last_year.drop(['id', 'station', 'date', 'tobs'], axis=1).reset_index()
+    prec_html = year_prec.to_dict(orient="split")
+
+    return jsonify(prec_html)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
 
     stations = meas_db.groupby("station").count().sort_values(["id"], ascending=False)
-    stations = stations.drop(['id', 'date', 'prcp', 'tobs'], axis=1)
-    stat_html = stations.to_json()
-    return stat_html
+    stations = stations.drop(['id', 'date', 'prcp', 'tobs'], axis=1).reset_index()
+    stat_html = stations.to_dict(orient="split")
+
+    return jsonify(stat_html)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
 
     act_stat = last_year.loc[last_year["station"] =='USC00519281']
-    act_stat = act_stat.drop(['id', 'date', 'station', 'tobs'], axis=1)
-    act_stat_html = act_stat.to_json()
+    act_stat = act_stat.drop(['id', 'date', 'station', 'tobs'], axis=1).reset_index()
+    act_stat_html = act_stat.to_dict(orient="split")
 
-    return act_stat_html
+    return jsonify(act_stat_html)
 
-# @app.route("/api/v1.0/start")
-# def start():
+@app.route("/api/v1.0/8-24-16_to_8-23-17")
+def start_to_end():
 
-#     last_year = date_sort.sort_index().last('365D')
-#     year_prec = last_year.drop(['id', 'station', 'date', 'tobs'], axis=1)
-#     prec_json = year_prec.to_html(classes="year_prec")
+    act_stat_1yr = last_year.loc[last_year["station"] =='USC00519281'].reset_index()
+    act_stat_1yr = act_stat_1yr["tobs"].describe().reset_index()
+    act_stat_1yr_dict = act_stat_1yr.to_dict(orient="split")
 
-# @app.route("/api/v1.0/start_to_end")
-# def start_to_end():
-
-#     last_year = date_sort.sort_index().last('365D')
-#     year_prec = last_year.drop(['id', 'station', 'date', 'tobs'], axis=1)
-#     prec_json = year_prec.to_html(classes="year_prec")
-
-    return prec_json   
+    return jsonify(act_stat_1yr_dict)   
 
 if __name__ == '__main__':
     app.run(debug=True)
